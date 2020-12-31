158221ef7a9cec7b90532ea398497dad13db1dfe
darshangit <darshans1989@gmail.com>
2020-12-30 20:24:08 -0500

    css changes completed + js function add - initia;

---
+++ js/dataFunctions.js
@@ -0,0 +1,12 @@
+export const getSearchTerm = () => {
+    const rawSearchTerm = document.getElementById("search").value.trim();
+    const regex = /[ ]{2,}/gi;
+    const searchTerm = rawSearchTerm.replaceAll(regex, " ");
+    return searchTerm;
+}
+
+export const retrieveSearchResults = async (searchTerm) => {
+    const wikiSearchString = getWikiSearchString(searchTerm);
+}
+
+const getWikiSearchString = (searchTerm)
