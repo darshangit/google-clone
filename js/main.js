4e84cc2dfaff0dc902f3535cad80ad1c93250e6e
darshangit <darshans1989@gmail.com>
2020-12-31 20:01:36 -0500

    more js functions added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { setSearchFocus } from "./searchBar.js"
-import { getSearchTerm } from "./dataFunctions.js";
+import { getSearchTerm, retrieveSearchResults } from "./dataFunctions.js";
+import { buildSearchResults } from './searchResults.js';
 
 document.addEventListener("readystatechange", (event) => {
     if (event.target.readyState === "complete") {
@@ -9,9 +10,7 @@ document.addEventListener("readystatechange", (event) => {
 
 const initApp = () => {
     setSearchFocus();
-
-    //3 listeners clear text
-
+    //TODO: 3 listeners clear text
     const form = document.getElementById("searchBar");
     form.addEventListener("submit", submitTheSearch);
 };
@@ -19,14 +18,16 @@ const initApp = () => {
 //Procedural "workflow" function
 const submitTheSearch = (event) => {
     event.preventDefault();
-    // delete search results
+    // TODO:delete search results
     processTheSearch();
     setSearchFocus();
 };
 
 const processTheSearch = async () => {
-    // clear the stats line
+    // TODO:clear the stats line
     const searchTerm = getSearchTerm();
     if (searchTerm === "") return;
     const resultArray = await retrieveSearchResults(searchTerm);
-}
+    if (resultArray.length) buildSearchResults(resultArray);
+    // TODO:set stats line
+};
